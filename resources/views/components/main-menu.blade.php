664761855b745f01a580e0c86e2e5d2974caca3d
Lazzytchik <Lazzytchik@users.noreply.github.com>
2021-11-15 10:20:32 +0300

    Migration addition menu component added switched to controller

---
+++ resources/views/components/main-menu.blade.php
@@ -0,0 +1,14 @@
+<nav class="main-menu">
+    <a href="{{ route('main') }}">
+        <span class="mx-3">Главная</span>
+    </a>
+    <a href="{{ route('main') }}">
+        <span class="mx-3">Карты</span>
+    </a>
+    <a href="{{ route('main') }}">
+        <span class="mx-3">Расписания</span>
+    </a>
+    <a href="{{ route('main') }}">
+        <span class="mx-3">О сервисе</span>
+    </a>
+</nav>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,8 +5,21 @@
     <title>@yield('title')</title>
     <script src="https://api-maps.yandex.ru/2.1/?apikey=6c6ea9bc-90c7-4296-b72c-b90182922a00&lang=ru_RU" type="text/javascript"></script>
     <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/resources/css/main.css">
 </head>
 <body>
+<div class="wrap">
+    <div class="header">
+        <div class="inner-wrap">
+            <div class="menu-block">
+                <x-main-menu></x-main-menu>
+            </div>
+        </div>
+    </div>
+    <div class="main">
+
+    </div>
+</div>
     <div class="mt-5 mb-4 text-center">
         <h1>@yield('title')</h1>
         @yield('content')
